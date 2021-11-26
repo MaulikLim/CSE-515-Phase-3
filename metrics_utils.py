9232dc05c9a43b4b14bb620135e21ffd97360c37
Arpit Prajapati <arpitprajapati306@gmail.com>
2021-11-26 13:25:05 -0700

    Division by zero bug for Misses and False Positives and some formatting

--- metrics_utils.py
+++ metrics_utils.py
@@ -18,9 +18,10 @@ def print_matrices(y_true: List[int], y_pred: List[int]) -> None:
     :type y_pred: List
     :type y_true: List
     """
+    eps = 1e-5
     TP, TN, FP, FN = _get_fp_misses(y_true, y_pred)
-    FPR = FP / (TN + FP)
-    misses = FN / (TP + FN)
+    FPR = FP / (TN + FP + eps)
+    misses = FN / (TP + FN + eps)
     with np.printoptions(precision=3, suppress=True, formatter={'float': '{: 0.2f}'.format}):
         print(f"False Positive rate(in percent) for each class:\n {FPR * 100}")
         print(f"Miss rate(in percent) for each class:\n {misses * 100}")
--- task1.py
+++ task1.py
@@ -13,8 +13,7 @@ import pdb
 import featureLoader
 import latentFeatureGenerator
 from metrics_utils import print_matrices
-from sklearn import tree;
-
+from sklearn import tree
 
 from arg_parser_util import Parser
 from tech.PCA import PCA
@@ -38,20 +37,21 @@ if data is not None:
     labels = [x.split("-")[1] for x in data[1]]
     # train classifier as given in the input
     classifier = args.classifier.lower()
-    #k=args.k
+    # k=args.k
     if classifier == 'ppr':
         # Train PPR
-        types_of_labels=["cc","jitter","neg","con","emboss","noise01","noise02","original","poster","rot","smooth","stipple"]
-        ppr = Personalised_Page_Rank(20,types_of_labels)
-        ppr.fit(features,labels)
+        types_of_labels = ["cc", "jitter", "neg", "con", "emboss", "noise01", "noise02", "original", "poster", "rot",
+                           "smooth", "stipple"]
+        ppr = Personalised_Page_Rank(20, types_of_labels)
+        ppr.fit(features, labels)
         test_data = latentFeatureGenerator.compute_latent_features(args.query_folder, args.feature_model, args.k)
         test_features = test_data[0]
         test_labels = [x.split("-")[1] for x in test_data[1]]
-        test_predicted_labels = ppr.predict(test_features,test_labels)
-        print(ppr.accuracy(test_predicted_labels,test_labels))
+        test_predicted_labels = ppr.predict(test_features, test_labels)
+        print(ppr.accuracy(test_predicted_labels, test_labels))
         # for test_image,test_predicted_label in test_predicted_labels.items():
         #     print(test_image,"->",test_predicted_label)
-        
+
         pass
     elif classifier == 'svm':
         # Train SVM
@@ -73,7 +73,7 @@ if data is not None:
         print_matrices(test_labels, test_predictions)
 
     else:
-         #Train decision tree
+        # Train decision tree
         label_map = {}
         labels_set = list(set(labels))
         reverse_map = {}
@@ -82,7 +82,7 @@ if data is not None:
             reverse_map[i] = label
         labels = [label_map[x] for x in labels]
 
-        dt = DecisionTree(features,np.array(labels))
+        dt = DecisionTree(features, np.array(labels))
         dt.train()
 
         test_data = latentFeatureGenerator.compute_latent_features(args.query_folder, args.feature_model, args.k)
@@ -90,7 +90,6 @@ if data is not None:
         test_labels = [x.split("-")[1] for x in test_data[1]]
 
         predict_labels = []
-        for i,feature in enumerate(test_features):
+        for i, feature in enumerate(test_features):
             predict_labels.append(reverse_map[dt.predict(feature)])
-        print_matrices(np.array(test_labels), np.array(predict_labels))
-        pass
+        print_matrices(test_labels, predict_labels)
